d4e2a8061cc627993d1d2bd5e42e9abdcf09713c
Rustam Rasulov <rusky@miirrr.com>
2022-01-03 23:24:36 +0500

    Исправление соединения с БД

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -15,29 +15,16 @@ public class Util {
     private static final String url = "jdbc:" + dbtype + "://" + dbHost + ":" + dbPort + "/" + dbSchema;
 
     private static Connection connection = null;
-    private static jm.task.core.jdbc.util.Util instance = null;
 
-    private Util() {
+    public static Connection getConnection() {
         try {
             if (connection == null || connection.isClosed()) {
                 connection = DriverManager.getConnection(url, user, password);
             }
-
         } catch (SQLException e) {
             e.printStackTrace();
         }
-    }
-
-    public static jm.task.core.jdbc.util.Util getInstance() {
-        if (instance == null) {
-            instance = new jm.task.core.jdbc.util.Util();
-        }
-        return instance;
-    }
-
-    public Connection getConnection() {
         return connection;
     }
-
 }
 
